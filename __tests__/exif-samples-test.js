7558e67c3229242f1159075f1be41b50bc054348
fisker Cheung <lionkay@gmail.com>
2021-05-06 17:57:29 +0800

    Meta tweak

--- __tests__/exif-samples-test.js
+++ __tests__/exif-samples-test.js
@@ -1,5 +1,5 @@
-import {readFileSync} from 'fs'
-import path from 'path'
+import {readFileSync} from 'node:fs'
+import path from 'node:path'
 // eslint-disable-next-line import/no-extraneous-dependencies
 import glob from 'fast-glob'
 

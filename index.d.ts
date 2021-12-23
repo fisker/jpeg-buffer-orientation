1a11c4042bd67a1406010699464e9a5dc4d8d4b2
fisker Cheung <lionkay@gmail.com>
2021-12-23 16:31:10 +0800

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,14 @@
 export type JpegBufferOrientation = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 
 /**
-Get orientation from a jpeg buffer.
+Get orientation of a jpeg file.
+
 @param buffer - ArrayBuffer of jpeg file.
+
 @example
 ```
+import getJpegBufferOrientation from 'jpeg-buffer-orientation';
+
 getJpegBufferOrientation(buffer)
 ```
 */

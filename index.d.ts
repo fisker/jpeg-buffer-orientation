4aef6bd03d84334063b7a7aa9c689962e1204b7a
fisker Cheung <lionkay@gmail.com>
2021-12-23 16:29:34 +0800

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,11 @@
-/**
- * 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
- */
 export type JpegBufferOrientation = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 
 /**
 Get orientation from a jpeg buffer.
-@param buffer - Jpeg Buffer.
+@param buffer - ArrayBuffer of jpeg file.
 @example
 ```
-import getJpegBufferOrientation from 'jpeg-buffer-orientation';
-const myJPEGFile = 'path/to/a/jpeg/file'
-// this time we use fetch to get a ArrayBuffer
-const response = await fetch(myJPEGFile)
-const buffer = await response.arrayBuffer()
-const orientation = getJpegBufferOrientation(buffer)
+getJpegBufferOrientation(buffer)
 ```
 */
 export default function getJpegBufferOrientation (buffer: ArrayBuffer): JpegBufferOrientation | void;
